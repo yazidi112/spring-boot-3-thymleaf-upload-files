e3627a32b2419822730290edfcfd0b26e2425824
yazidi.imran <yazidi.imran@gmail.com>
2023-06-21 12:13:14 +0100

    init commit

---
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import com.example.demo.files.StorageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Demo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Application.class, args);
+    }
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
